36c928a951aa7c1d44ba9f8590754fbaf4c0d73e
Sibgathullah-Mohaideen <122789612+Sibgathullah-Mohaideen@users.noreply.github.com>
2024-09-25 21:33:06 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,5 @@ set -e
   
 # Stop the running container (if any)
 #echo "Done"
-container_id='docker ps | awk -F" "'{Print $1}'
+container_id='docker ps | awk -F" "'{Print $1}''
 docker rm -f$container_id
